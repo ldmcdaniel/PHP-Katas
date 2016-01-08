352eee4f5d8e0bceac17855eaacfa98aab34b3e4
Lucas McDaniel <lucas.mcdaniel@bernardhealth.com>
2016-01-08 14:30:23 -0600

    Final refactoring and working tests

---
+++ src/PrimeFactors.php
@@ -0,0 +1,22 @@
+<?php
+
+use PhpSpec\ObjectBehavior;
+
+class PrimeFactors extends ObjectBehavior
+{
+    public function generate($number)
+    {
+        $primes = [];
+
+        for ($candidate = 2; $number > 1; $candidate++)
+        {
+            for (; $number % $candidate == 0; $number /= $candidate)
+            {
+                $primes[] = $candidate;
+            }
+
+        }
+
+         return $primes;
+    }
+}
