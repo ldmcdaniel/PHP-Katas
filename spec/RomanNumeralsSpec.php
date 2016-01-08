61597f65c62b08b13be7c62548543ee131ddac9d
Lucas McDaniel <lucas.mcdaniel@bernardhealth.com>
2016-01-08 15:23:42 -0600

    Initial commit with a little bit of progress

--- spec/RomanNumeralsSpec.php
+++ spec/RomanNumeralsSpec.php
@@ -7,8 +7,28 @@ use Prophecy\Argument;
 
 class RomanNumeralsSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_calculates_the_roman_numberal_for_1()
     {
-        $this->shouldHaveType('RomanNumerals');
+        $this->convert(1)->shouldReturn('I');
+    }
+
+    function it_calculates_the_roman_numberal_for_2()
+    {
+        $this->convert(2)->shouldReturn('II');
+    }
+
+    function it_calculates_the_roman_numberal_for_5()
+    {
+        $this->convert(5)->shouldReturn('V');
+    }
+
+    function it_calculates_the_roman_numberal_for_6()
+    {
+        $this->convert(6)->shouldReturn('VI');
+    }
+
+    function it_calculates_the_roman_numberal_for_10()
+    {
+        $this->convert(10)->shouldReturn('X');
     }
 }
--- src/RomanNumerals.php
+++ src/RomanNumerals.php
@@ -2,4 +2,18 @@
 
 class RomanNumerals
 {
+    public function convert($number)
+    {
+        $solution = '';
+
+        if ($number >= 5)
+        {
+            $solution .= 'V';
+            $number -= 5;
+        }
+
+        $solution .= str_repeat('I', $number);
+
+         return $solution;
+    }
 }
