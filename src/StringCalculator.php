f364a7230c08e3bc7628aba22cf0c6efd6cae840
Lucas McDaniel <lucas.mcdaniel@bernardhealth.com>
2016-01-18 09:16:43 -0600

    Intermediate commit before finishing kata

---
+++ src/StringCalculator.php
@@ -0,0 +1,21 @@
+<?php
+
+class StringCalculator
+{
+    public function add($numbers)
+    {
+        $numbers = preg_split('/\s*(,|\\\n)\s*/', $numbers);
+
+        $solution = 0;
+
+        foreach ($numbers as $number)
+        {
+            if ($number < 0) throw new InvalidArgumentException('Invalid number provider: -2');
+            if ($number >= 1000) continue;
+
+            $solution += $number;
+        }
+
+        return $solution;
+    }
+}
